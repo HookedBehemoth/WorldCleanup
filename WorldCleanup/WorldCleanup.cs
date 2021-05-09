19ef15a6fd3e0525684609aa53fd03eea4fa3e87
Luis Scheurenbrand <LuisScheurenbrand@gmail.com>
2021-05-09 22:46:36 +0200

    Add GPLv3 license

--- WorldCleanup/WorldCleanup.cs
+++ WorldCleanup/WorldCleanup.cs
@@ -1,4 +1,20 @@
-﻿using MelonLoader;
+﻿/*
+ * Copyright (c) 2021 HookedBehemoth
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using MelonLoader;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.SceneManagement;
