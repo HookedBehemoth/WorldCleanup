e1030cac7125db6cb692d8d430d6fdf92ca71c13
HookedBehemoth <LuisScheurenbrand@gmail.com>
2022-04-23 15:16:56 +0200

    ignore null strings

--- Sigh.cs
+++ Sigh.cs
@@ -2,27 +2,30 @@
 /* Note: While loading a 400KiB string isn't really a    */
 /*       problem, trying to render that to a texture is. */
 
-namespace WorldCleanup {
-    public static class ZipBombExtend {
-        public static string Truncate(this string value, int max_length) {
+namespace WorldCleanup
+{
+    public static class ZipBombExtend
+    {
+        public static string Truncate(this string value, int max_length)
+        {
             if (!string.IsNullOrEmpty(value) && value.Length > max_length)
                 return value.Substring(0, max_length) + "…";
             return value;
         }
 
         public static string TrucatedName(this VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionParameters.Parameter param)
-            => param.name.Truncate(32);
+            => param.name?.Truncate(32);
 
         public static string TruncatedName(this VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control control)
-            => control.name.Truncate(32);
+            => control.name?.Truncate(32);
 
         public static string TruncatedName(this VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control.Parameter param)
-            => param.name.Truncate(32);
+            => param.name?.Truncate(32);
 
         public static string TruncatedName(this VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control.Label label)
-            => label.name.Truncate(32);
+            => label.name?.Truncate(32);
 
         public static string TruncatedName(this VRC.Playables.AvatarParameter param)
-            => param.field_Private_String_0.Truncate(32);
+            => param.field_Private_String_0?.Truncate(32);
     }
 }
